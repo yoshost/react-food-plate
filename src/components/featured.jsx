fdeb0f4f0c9e26994bc6eb5f1a2d113ef33e4a6d
DevYmA <ymamarathunga@gmail.com>
2022-07-13 14:15:47 +0530

    add style

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
+import styled from 'styled-components';
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/react-splide/css';
 
 
-function Featured(){
+
+
+function Featured() {
     const [featured, setFeatured] = useState([]);
 
     useEffect(() => {
         getFeaturedRecipe();
-    },[]);
+    }, []);
 
     const getFeaturedRecipe = async () => {
         const URL = `${process.env.REACT_APP_RECIPE_URL}/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5`;
@@ -16,21 +21,48 @@ function Featured(){
         setFeatured(data.recipes);
     }
 
-    
 
-    return(
+
+    return (
         <div>
-            {
-                featured.map((recipe, index) => {
-                    return (
-                        <div key={index}>
-                            <h5>{recipe.title}</h5>
-                        </div>
-                    )
-                })
-            }
+            <Splide options={{
+                perPage: 4
+            }}>
+                {
+                    featured.map((recipe, index) => {
+                        return (
+                            <SplideSlide>
+                                <Container key={index}>
+                                    <Card>
+                                        <h1>{recipe.title}</h1>
+                                        <img src={recipe.image} alt={recipe.title} />
+                                    </Card>
+                                </Container>
+                            </SplideSlide>
+                        )
+                    })
+                }
+            </Splide>
         </div>
     )
 }
+
+const Container = styled.div`
+    margin: 1em;
+    padding: 1em;
+    border-radius: 6px;
+`
+
+const Card = styled.div`
+    min-height: 25rem;
+    margin: 2em;
+    overflow: hidden;
+    img{
+        border-radius: 6px;
+    }
+    h1{
+        font-size: 1em;
+    }
+`
 
 export default Featured;
